4c63dd78b487546dd6c5047c8182efdf5bb0ea22
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2021-07-27 22:09:30 +0700

    Update script

--- run_1.sh
+++ run_1.sh
@@ -2,7 +2,7 @@
 export TIMES=61
 
 echo "THU DUC"
-for i in {1..50}
+for i in {1..30}
   do
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "THU DUC" --img_note _THU_DUC_$i
     echo $i
@@ -10,7 +10,7 @@ for i in {1..50}
   done
 
 echo "GO VAP"
-for i in {1..50}
+for i in {1..30}
   do
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "GO VAP" --img_note _GO_VAP_$i
     echo $i
@@ -18,21 +18,21 @@ for i in {1..50}
   done
 
 echo "BINH THANH"
-for i in {1..50}
+for i in {1..30}
   do
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "BINH THANH" --img_note _BINH_THANH_$i
     echo '\n'
   done
 
 echo "BINH CHANH"
-for i in {1..50}
+for i in {1..30}
   do
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "BINH CHANH" --img_note _BINH_CHANH_$i
     echo '\n'
   done
 
 echo "BINH TAN"
-for i in {1..50}
+for i in {1..30}
   do
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "BINH TAN" --img_note _BINH_TAN_$i
     echo '\n'
