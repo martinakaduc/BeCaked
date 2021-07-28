537909ceffb83f2c347b86c8fce3807de70393ed
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2021-07-28 18:41:45 +0700

    Update script

--- run_0.sh
+++ run_0.sh
@@ -37,3 +37,47 @@ for i in {1..30}
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "QUAN 8" --img_note _QUAN_8_$i
     echo '\n'
   done
+
+echo "QUAN 3"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "QUAN 3" --img_note _QUAN_3_$i
+    echo $i
+    echo '\n'
+  done
+
+echo "QUAN 5"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "QUAN 5" --img_note _QUAN_5_$i
+    echo $i
+    echo '\n'
+  done
+
+echo "QUAN 6"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "QUAN 6" --img_note _QUAN_6_$i
+    echo '\n'
+  done
+
+echo "QUAN 11"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "QUAN 11" --img_note _QUAN_12_$i
+    echo '\n'
+  done
+
+echo "QUAN 12"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "QUAN 12" --img_note _QUAN_12_$i
+    echo '\n'
+  done
+
+echo "TAN PHU"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "TAN PHU" --img_note _TAN_PHU_$i
+    echo '\n'
+  done
--- run_1.sh
+++ run_1.sh
@@ -37,3 +37,47 @@ for i in {1..30}
     python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "BINH TAN" --img_note _BINH_TAN_$i
     echo '\n'
   done
+
+echo "CAN GIO"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "CAN GIO" --img_note _CAN_GIO_$i
+    echo $i
+    echo '\n'
+  done
+
+echo "CU CHI"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "CU CHI" --img_note _CU_CHI_$i
+    echo $i
+    echo '\n'
+  done
+
+echo "HOC MON"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "HOC MON" --img_note _HOC_MON_$i
+    echo '\n'
+  done
+
+echo "NHA BE"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "NHA BE" --img_note _NHA_BE_$i
+    echo '\n'
+  done
+
+echo "PHU NHUAN"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "PHU NHUAN" --img_note _PHU_NHUAN_$i
+    echo '\n'
+  done
+
+echo "TAN BINH"
+for i in {1..30}
+  do
+    python evaluation.py  --level 0 --day_lag 10 --start_date 43 --end_date $TIMES --plot_prediction 1 --infer_date 7 --start_train_date 0 --end_train_date $TIMES --cuda 1 --ward "TAN BINH" --img_note _TAN_BINH_$i
+    echo '\n'
+  done
